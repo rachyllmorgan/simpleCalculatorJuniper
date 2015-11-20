a88ba22fdae20316d618928b438bc20884a3ad1e
Rachel Morgan <rachyllmorgan@gmail.com>
2015-11-19 21:49:26 -0600

    Parse class/test completed, working on evaluate

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleCalculator;
+
+namespace SimpleCalculator
+{
+    public class Evaluate
+    {
+        public char DetermineEvaluator(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Addition(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
